141d1089fc98b4e86a79f017fc8cb8cd6b35ed40
IgnacioG2000 <nachog.2000@hotmail.com>
2024-07-13 19:46:21 -0300

    Cree los componentes del proyecto

---
+++ frontend/src/components/ListaUsuarios.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ListaUsuarios = () => {
+    return (
+        <div>
+            {/* Your component code here */}
+        </div>
+    );
+};
+
+export default ListaUsuarios;
