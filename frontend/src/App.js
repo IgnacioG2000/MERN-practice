608b415bf13ab28b2635c5029756c0cd4e1c8bf6
IgnacioG2000 <nachog.2000@hotmail.com>
2024-07-16 15:53:37 -0300

    Menu de navegacion creado e instalado el router dom

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,22 @@
 import './App.css';
+import {Routes, Route} from 'react-router-dom';
+
+import Navegacion from './components/Navegacion';
+import ListaUsuarios from './components/ListaUsuarios';
+import CrearUsuarios from './components/CrearUsuarios';
 
 function App() {
   return (
     <div className="">
-      <h2 className='text-center'>Hola Mundo</h2>
-    </div>
+      <Navegacion />
+      <div className="container p-4">
+        <Routes>
+          <Route path="/" element={<ListaUsuarios />} />
+          <Route path="/creacionUsuario" element={<CrearUsuarios />} />
+          <Route path='/edicionUsuario/:id' element={<CrearUsuarios />} />
+        </Routes>
+        </div>
+      </div>
   );
 }
 
--- frontend/src/components/ListaUsuarios.js
+++ frontend/src/components/ListaUsuarios.js
@@ -3,7 +3,7 @@ import React from 'react';
 const ListaUsuarios = () => {
     return (
         <div>
-            {/* Your component code here */}
+            Lista Usuario
         </div>
     );
 };
