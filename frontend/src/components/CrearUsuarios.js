c5d477c69392560c5d97d177401dcf057df3a73c
IgnacioG2000 <nachog.2000@hotmail.com>
2024-07-16 22:01:52 -0300

    Petición get funcionando

--- frontend/src/components/CrearUsuarios.js
+++ frontend/src/components/CrearUsuarios.js
@@ -1,10 +1,137 @@
 import React from 'react';
+// axios es una biblioteca de JavaScript utilizada para hacer solicitudes HTTP desde el navegador y Node.js. 
+//Es útil para interactuar con APIs, recuperar datos de servidores y enviar datos a servidores.
+import axios from 'axios';
 
 const CrearUsuarios = () => {
+
+    const valorInicial = {
+        nombre: '',
+        apellido: '',
+        edad: 18,
+        telefono: 0,
+        correo: ''
+    }
+
+    // Crear estado
+    // usuario = estado
+    // setUsuario = funcion para actualizar el estado
+    // useState(valorInicial) = inicializar el estado
+    const [usuario, setUsuario] = React.useState(valorInicial);
+
+    // Capturar datos de los inputs
+    // e = evento
+    // name = nombre del input
+    // value = valor del input
+    // setUsuario = funcion para actualizar el estado
+    // ...usuario = copia del estado actual
+    // [name]: value = actualiza el valor del input
+    const capturarDatos = (e) => {
+        const {name, value} = e.target;
+        setUsuario({...usuario, [name]: value});
+    }
+
+    const guardarDatos = async(e) => {
+        // Evitar que se recargue la pagina
+        e.preventDefault();
+        //console.log(usuario);
+
+        // Enviar los datos al backend
+        const nuevoUsuario = {
+            nombre: usuario.nombre,
+            apellido: usuario.apellido,
+            edad: usuario.edad,
+            telefono: usuario.telefono,
+            correo: usuario.correo
+        }
+
+        await axios.post('http://localhost:4000/api/usuarios', nuevoUsuario);
+
+
+        // Limpiar los inputs
+        setUsuario({...valorInicial});
+    }
+
+
     return (
-        <div>
-            Crear Usuario
-        </div>
+      <div className="col-md-6 offset-md-3">
+        <form onSubmit={guardarDatos}>
+            <h2 className='text-center mb-3'>Crear Usuario</h2>
+          <div className="card card-body">
+            <div className="mb-3">
+              <label>Nombre</label>
+
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Ingresar el nombre del usuario"
+                required
+                name='nombre'
+                value={usuario.nombre}
+                onChange={capturarDatos}
+              />
+            </div>
+
+            <div className="mb-3">
+              <label>Apellido</label>
+
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Ingresar el apellido del usuario"
+                required
+                name='apellido'
+                value={usuario.apellido}
+                onChange={capturarDatos}
+              />
+            </div>
+
+            <div className="mb-3">
+              <label>Edad</label>
+
+              <input
+                type="number"
+                className="form-control"
+                placeholder="Ingresar la edad del usuario"
+                required
+                name='edad'
+                value={usuario.edad}
+                onChange={capturarDatos}
+              />
+            </div>
+
+            <div className="mb-3">
+              <label>Teléfono</label>
+
+              <input
+                type="number"
+                className="form-control"
+                placeholder="Ingresar el teléfono del usuario"
+                required
+                name='telefono'
+                value={usuario.telefono}
+                onChange={capturarDatos}
+              />
+            </div>
+
+            <div className="mb-3">
+              <label>Correo</label>
+
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Ingresar el correo electrónico del usuario"
+                required
+                name='correo'
+                value={usuario.correo}
+                onChange={capturarDatos}
+              />
+            </div>
+
+            <button className="btn btn-primary form-control">Crear Usuario</button>
+          </div>
+        </form>
+      </div>
     );
 };
 
--- frontend/src/components/ListaUsuarios.js
+++ frontend/src/components/ListaUsuarios.js
@@ -1,10 +1,48 @@
 import React from 'react';
+import axios from 'axios';
+import { useEffect } from 'react';
 
 const ListaUsuarios = () => {
+
+
+    // useState([]) = inicializar el estado
+    // [] = valor inicial del estado
+    const [usuarios, setUsuarios] = React.useState([]);
+
+    // useEffect = hook para realizar peticiones asincronas
+    useEffect(() => {
+        // Funcion asincrona para obtener los usuarios -> peticion get a la ruta de los usuarios
+        // setUsuarios(response.data) = actualizar el estado con los datos de la peticion
+        //
+        const getUsuarios = async () => {
+            const response = await axios.get('http://localhost:4000/api/usuarios');
+            setUsuarios(response.data);
+        }
+        getUsuarios();
+    }, [usuarios])
+
     return (
-        <div>
-            Lista Usuario
-        </div>
+      <div className="row">
+        {usuarios.map((usuario) => (
+          <div className="col-md-4 p-2" key={usuario._id}>
+            <div className="card">
+              <div className="card-header">
+                <h5>Nombre: {usuario.nombre}</h5>
+              </div>
+              <div className="card-body">
+                <p>Apellido: {usuario.apellido}</p>
+                <p>Edad: {usuario.edad}</p>
+                <p>Teléfono: {usuario.telefono}</p>
+                <p>Correo: {usuario.correo}</p>
+              </div>
+
+              <div className="card-footer">
+                <button className="btn btn-danger">Eliminar</button>
+                </div>
+            </div>
+          </div>
+        ))}
+      </div>
     );
 };
 
